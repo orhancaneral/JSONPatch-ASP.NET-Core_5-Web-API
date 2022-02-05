8e542ffcd459b86333975fbb08ce290a5a782d7e
orhancan eral <orhancaneral@hotmail.com>
2022-02-05 10:35:33 +0300

    9

--- scr/JsonPatchWebApi/Services/DataService.cs
+++ scr/JsonPatchWebApi/Services/DataService.cs
@@ -7,6 +7,13 @@ namespace JsonPatchWebApi.Services
 {
     public class DataService : IDataService
     {
+        public DataService()
+        {
+            _people.Add(new Person() { Id = Guid.NewGuid(), Name = "Orhan", Age = 30 });
+            _people.Add(new Person() { Id = Guid.NewGuid(), Name = "Rabia", Age = 28 });
+            _people.Add(new Person() { Id = Guid.NewGuid(), Name = "Irmak Masal", Age = 0 });
+        }
+
         private List<Person> _people = new List<Person>();
 
         public Guid Add(Person person)
